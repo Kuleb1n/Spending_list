b1669df19e1e05d5986867160d374a9c720090a3
Kuleb1n <v_vpyt@mail.ru>
2023-06-25 15:48:03 +0300

    initial commit

---
+++ Spending_list/apps/spending_operations/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SpendingOperationsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Spending_list.apps.spending_operations'
