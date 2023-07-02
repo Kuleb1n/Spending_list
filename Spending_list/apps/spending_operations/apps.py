101bb28103a64cfe57628cd4f4d683a2fb7cd59d
Kuleb1n <v_vpyt@mail.ru>
2023-07-02 23:08:32 +0300

    adding verbose_name = 'Операции'

--- Spending_list/apps/spending_operations/apps.py
+++ Spending_list/apps/spending_operations/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SpendingOperationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Spending_list.apps.spending_operations'
+    verbose_name = 'Операции'
