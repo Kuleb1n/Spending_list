28e9a2992ad0019b68da15f148540efc05876eb8
Kuleb1n <v_vpyt@mail.ru>
2023-07-02 23:09:08 +0300

    registration of models in the admin panel

--- Spending_list/apps/spending_operations/admin.py
+++ Spending_list/apps/spending_operations/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Operation, Goal
+
+
+@admin.register(Operation)
+class OperationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'cost', 'operation_at')
+    list_display_links = ('name',)
+    list_filter = ('operation_at',)
+    search_fields = ('name',)
+    ordering = ('-operation_at',)
+
+
+@admin.register(Goal)
+class GoalAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'goal_type', 'start_date', 'finish_date', 'value',)
+    list_display_links = ('name',)
+    search_fields = ('name',)
