03e9f5aa839f8dafb3a4b4d40bfcf335b44f0048
Kuleb1n <v_vpyt@mail.ru>
2023-07-05 21:49:14 +0300

    registration of a category model in the admin panel

--- Spending_list/apps/spending_operations/admin.py
+++ Spending_list/apps/spending_operations/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 
-from .models import Operation, Goal
+from .models import Operation, Goal, Category
 
 
 @admin.register(Operation)
 class OperationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description', 'cost', 'operation_at')
-    list_display_links = ('name',)
-    list_filter = ('operation_at',)
+    list_display = ('id', 'name', 'description', 'category', 'cost', 'operation_at')
+    list_display_links = ('name', 'category')
+    list_filter = ('operation_at', 'category')
     search_fields = ('name',)
     ordering = ('-operation_at',)
 
@@ -15,5 +15,12 @@ class OperationAdmin(admin.ModelAdmin):
 @admin.register(Goal)
 class GoalAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'description', 'goal_type', 'start_date', 'finish_date', 'value',)
+    list_display_links = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
     list_display_links = ('name',)
     search_fields = ('name',)
