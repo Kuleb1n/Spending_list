6836ac64eb5b95031ae24c40170b409fcb118810
Kuleb1n <v_vpyt@mail.ru>
2023-07-11 19:04:31 +0300

    creating a new custom model

--- Spending_list/apps/users/models.py
+++ Spending_list/apps/users/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
+from django.core import validators
 from django.db import models
+from django.utils.translation import gettext_lazy
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(
+        gettext_lazy("Email address"),
+        unique=True,
+        validators=[validators.validate_email],
+        error_messages={
+            "unique": gettext_lazy("A user with that email already exists."),
+        },
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return f'{self.email}'
