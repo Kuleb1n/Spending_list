4d3c967d8ce1552c992273ed6b1d3cb82181a963
Kuleb1n <v_vpyt@mail.ru>
2023-07-11 19:32:28 +0300

    registration of the user's model in the admin panel

--- Spending_list/apps/users/admin.py
+++ Spending_list/apps/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "email", "first_name", "last_name", "is_staff", "is_active", "date_joined")
+    list_display_links = ("id", "username", "email")
+    list_filter = ("date_joined", "is_staff", "is_active")
+    search_fields = ("email", "date_joined")
