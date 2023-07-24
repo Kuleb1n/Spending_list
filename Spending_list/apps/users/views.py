5e186ad4fa11ce8e56569f58297e6724ad24cf20
Kuleb1n <v_vpyt@mail.ru>
2023-07-24 20:43:38 +0300

    adding class RegisterUserView

--- Spending_list/apps/users/views.py
+++ Spending_list/apps/users/views.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.views import LoginView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from Spending_list.apps.users.models import User
+from apps.users.forms import RegisterUserForm
 
 
 class LoginUserView(LoginView):
@@ -7,3 +12,11 @@ class LoginUserView(LoginView):
 
     def get_redirect_url(self):
         return reverse_lazy('main-page')
+
+
+class RegisterUserView(SuccessMessageMixin, CreateView):
+    model = User
+    template_name = 'user/login.html'
+    form_class = RegisterUserForm
+    success_url = reverse_lazy('login')
+    success_message = 'You have successfully registered!'
