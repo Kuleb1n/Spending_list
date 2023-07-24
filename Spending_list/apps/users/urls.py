b295de96af23b87b57ac57c2535c446bef353c11
Kuleb1n <v_vpyt@mail.ru>
2023-07-24 20:43:13 +0300

    adding path -> 'register/'

--- Spending_list/apps/users/urls.py
+++ Spending_list/apps/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from apps.users.views import LoginUserView
+from apps.users.views import LoginUserView, RegisterUserView
 
 urlpatterns = (
     path('login/', LoginUserView.as_view(), name='login'),
+    path('register/', RegisterUserView.as_view(), name='register'),
 )
