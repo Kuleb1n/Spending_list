1ff7f8831373c32e6b823cc21b4a1b1165409fe3
Kuleb1n <v_vpyt@mail.ru>
2023-07-28 19:46:23 +0300

    adding path 'email-confirmation/<str:email>/<uuid:link>/'

--- Spending_list/apps/users/urls.py
+++ Spending_list/apps/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from Spending_list.apps.users.views import LoginUserView, RegisterUserView
+from Spending_list.apps.users.views import LoginUserView, RegisterUserView, EmailConfirmationView
 
 urlpatterns = (
     path('login/', LoginUserView.as_view(), name='login'),
     path('register/', RegisterUserView.as_view(), name='register'),
+    path('email-confirmation/<str:email>/<uuid:link>/', EmailConfirmationView.as_view(), name='email-confirmation'),
 )
