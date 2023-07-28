68e8e87190f5fc9b33948bf7cb00d0b6aaf2e07b
Kuleb1n <v_vpyt@mail.ru>
2023-07-28 19:48:57 +0300

    changing the behavior of the form after registration

--- Spending_list/apps/users/forms.py
+++ Spending_list/apps/users/forms.py
@@ -1,9 +1,24 @@
-from django.contrib.auth.forms import UserCreationForm
+import datetime
+import uuid
 
-from Spending_list.apps.users.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.utils.timezone import now
+
+from Spending_list.apps.users.models import User, EmailConfirmation
 
 
 class RegisterUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(RegisterUserForm, self).save(commit=True)
+        link = uuid.uuid4()
+        expiration_of_time = now() + datetime.timedelta(days=2)
+        email_confirmation = EmailConfirmation.objects.create(
+            link=link,
+            user=user,
+            expiration_of_time=expiration_of_time)
+        email_confirmation.sending_confirmation_by_email()
+        return user
