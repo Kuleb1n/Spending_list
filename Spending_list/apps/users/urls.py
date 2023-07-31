e68d08bf820dbc4659f7d3a4311bc6600dce6bb2
Kuleb1n <v_vpyt@mail.ru>
2023-07-31 14:06:54 +0300

    path('logout/'...)

--- Spending_list/apps/users/urls.py
+++ Spending_list/apps/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from Spending_list.apps.users.views import LoginUserView, RegisterUserView, EmailConfirmationView
+from Spending_list.apps.users.views import *
 
 urlpatterns = (
     path('login/', LoginUserView.as_view(), name='login'),
     path('register/', RegisterUserView.as_view(), name='register'),
+    path('logout/', user_logout, name='logout'),
     path('email-confirmation/<str:email>/<uuid:link>/', EmailConfirmationView.as_view(), name='email-confirmation'),
 )
