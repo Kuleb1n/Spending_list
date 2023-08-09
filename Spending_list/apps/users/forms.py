59601c13b3d52f8ddc81b691b52f8b6d38e08803
Kuleb1n <v_vpyt@mail.ru>
2023-08-09 23:20:46 +0300

    redefining the user authentication form

--- Spending_list/apps/users/forms.py
+++ Spending_list/apps/users/forms.py
@@ -1,10 +1,43 @@
 import datetime
 import uuid
-
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.core.exceptions import ValidationError
 from django.utils.timezone import now
 
 from Spending_list.apps.users.models import User, EmailConfirmation
+
+
+class UserAuthenticationForm(AuthenticationForm):
+
+    def __init__(self, request, *args, **kwargs):
+        super().__init__(request, *args, **kwargs)
+
+    def clean(self):
+        email = self.cleaned_data.get("username")
+        password = self.cleaned_data.get("password")
+
+        if email is not None and password:
+            self.user_cache = authenticate(
+                self.request, email=email, password=password
+            )
+            if len(User.objects.filter(email=email)) == 1 and not self.user_cache:
+                user = User.objects.get(email=email)
+                if user.is_active in (False,):
+                    self.confirm_login_allowed(self.user_cache)
+                else:
+                    raise self.get_invalid_login_error()
+
+            else:
+                self.get_invalid_login_error()
+
+        return self.cleaned_data
+
+    def confirm_login_allowed(self, user):
+        raise ValidationError(
+            self.error_messages["inactive"],
+            code="inactive",
+        )
 
 
 class RegisterUserForm(UserCreationForm):
